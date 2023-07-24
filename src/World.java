b94f49e9bf957f8fbc536751490aeaa0e6e875bf
Eeshan Mishra <55258933+EeshanMishra@users.noreply.github.com>
2023-07-24 02:24:40 -0700

    Update World.java

--- src/World.java
+++ src/World.java
@@ -1,2 +1,38 @@
 public class World {
+    private Cell[][] cells;
+    private int size;
+
+    public World(int size) {
+        this.size = size;
+        this.cells = new Cell[size][size];
+        initializeGrid();
+    }
+
+    private void initializeGrid() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                cells[i][j] = new Cell();
+            }
+        }
+    }
+
+    public Cell getCell(int x, int y) {
+        return cells[x][y];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder gridString = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                gridString.append(cells[i][j].isPainted() ? "P " : "- ");
+            }
+            gridString.append("\n");
+        }
+        return gridString.toString();
+    }
 }
