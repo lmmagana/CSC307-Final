7ab8401edf607175d889997dfa72ed9b687ead88
Triet Tran <ptran6@apple.com>
2023-07-24 13:46:39 -0500

    draw grid dynamically

--- src/World.java
+++ src/World.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class World extends JPanel {
+
     private Cell[][] cells;
     private int size;
+    private int cellSize = 60;
 
     public World(int size) {
         this.size = size;
         this.cells = new Cell[size][size];
         initializeGrid();
+        setBackground(Color.BLACK);
     }
 
     private void initializeGrid() {
@@ -24,6 +28,26 @@ public class World extends JPanel {
 
     public int getGridSize() {
         return size;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawGrid(g);
+    }
+
+    private void drawGrid(Graphics g) {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                int x = col * cellSize;
+                int y = row * cellSize;
+                g.setColor(Color.WHITE);
+                g.drawRect(x, y, cellSize, cellSize);
+                if (cells[row][col].isPainted()) {
+                    g.fillRect(x + 1, y + 1, cellSize - 1, cellSize - 1);
+                }
+            }
+        }
     }
 
     @Override
--- src/WorldPanel.java
+++ src/WorldPanel.java
@@ -7,53 +7,26 @@ import java.awt.event.ActionListener;
 
 public class WorldPanel extends JPanel implements ActionListener, ChangeListener {
 
-  private int gridSize = 5; // Set the size of the grid (5x5)
+    public WorldPanel() {
+        setLayout(null);
 
-  public WorldPanel() {
-    // left side
-    // panel with the world buttons
-    // JLabel gridPlaceholder = new JLabel("Grid goes here.");
-    // add(gridPlaceholder);
-  }
+        int gridSize = 5;
+        int cellSize = 60;
+        int xOffset = 20;
+        int yOffset = 10;
 
-  @Override
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    drawGrid(g);
-  }
-
-  private void drawGrid(Graphics g) {
-    int cellSize = 60;
-    int xOffset = 20;
-    g.setColor(Color.WHITE);
-    // Draw the vertical grid lines
-    for (int col = 0; col <= gridSize; col++) {
-      int x = xOffset + col * cellSize;
-      g.drawLine(x, 0, x, gridSize * cellSize);
+        World world = new World(gridSize);
+        world.setBounds(xOffset, yOffset, gridSize * cellSize, gridSize * cellSize);
+        add(world);
     }
-    // Draw the horizontal grid lines
-    for (int row = 0; row <= gridSize; row++) {
-      int y = row * cellSize;
-      g.drawLine(xOffset, y, gridSize * cellSize + xOffset, y);
-    }
-    g.setColor(Color.BLACK);
-    // Fill each grid cell with black color
-    for (int row = 0; row < gridSize; row++) {
-      for (int col = 0; col < gridSize; col++) {
-        int x = xOffset + col * cellSize + 1;
-        int y = row * cellSize + 1;
-        g.fillRect(x, y, cellSize - 1, cellSize - 1);
-      }
-    }
-  }
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    // Handle button clicks or other actions if needed
-  }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Handle button clicks or other actions if needed
+    }
 
-  @Override
-  public void stateChanged(ChangeEvent e) {
-    // Handle state changes of components if needed
-  }
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        // Handle state changes of components if needed
+    }
 }
