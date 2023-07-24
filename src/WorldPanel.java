1b6ecdb4fedd827e5046fa4a7561e86d25b9a1fa
Triet Tran <ptran6@apple.com>
2023-07-24 13:20:56 -0500

    update drawgrid

--- src/WorldPanel.java
+++ src/WorldPanel.java
@@ -7,31 +7,53 @@ import java.awt.event.ActionListener;
 
 public class WorldPanel extends JPanel implements ActionListener, ChangeListener {
 
-    public WorldPanel(){
+  private int gridSize = 5; // Set the size of the grid (5x5)
 
-        // left side
-        // panel with the world buttons
-        /*JLabel gridPlaceholder = new JLabel("Grid goes here.");
-        add(gridPlaceholder);
-        */
+  public WorldPanel() {
+    // left side
+    // panel with the world buttons
+    // JLabel gridPlaceholder = new JLabel("Grid goes here.");
+    // add(gridPlaceholder);
+  }
 
-        setLayout(null);
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    drawGrid(g);
+  }
 
-        World world = new World(5);
-        world.setBounds(40, 10, 300, 300);
-        world.setBackground(Color.BLACK);
-        add(world);
-
-
+  private void drawGrid(Graphics g) {
+    int cellSize = 60;
+    int xOffset = 20;
+    g.setColor(Color.WHITE);
+    // Draw the vertical grid lines
+    for (int col = 0; col <= gridSize; col++) {
+      int x = xOffset + col * cellSize;
+      g.drawLine(x, 0, x, gridSize * cellSize);
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+    // Draw the horizontal grid lines
+    for (int row = 0; row <= gridSize; row++) {
+      int y = row * cellSize;
+      g.drawLine(xOffset, y, gridSize * cellSize + xOffset, y);
     }
-
-    @Override
-    public void stateChanged(ChangeEvent e) {
-
+    g.setColor(Color.BLACK);
+    // Fill each grid cell with black color
+    for (int row = 0; row < gridSize; row++) {
+      for (int col = 0; col < gridSize; col++) {
+        int x = xOffset + col * cellSize + 1;
+        int y = row * cellSize + 1;
+        g.fillRect(x, y, cellSize - 1, cellSize - 1);
+      }
     }
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // Handle button clicks or other actions if needed
+  }
+
+  @Override
+  public void stateChanged(ChangeEvent e) {
+    // Handle state changes of components if needed
+  }
 }
