28856db689d38f1f07c3afdcb36817507fa8e5fd
Eeshan Mishra <55258933+EeshanMishra@users.noreply.github.com>
2023-07-24 02:26:11 -0700

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,33 @@
 public class Cell {
+    private boolean painted;
+    private String color;
+
+    public Cell() {
+        this.painted = false;
+        this.color = "None"; // Default color is None, indicating that the cell is not painted.
+    }
+
+    public boolean isPainted() {
+        return painted;
+    }
+
+    public void setPainted(boolean painted) {
+        this.painted = painted;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "painted=" + painted +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
