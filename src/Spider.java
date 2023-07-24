bb970b28aa6044344ed2365a747196276666c490
Eeshan Mishra <55258933+EeshanMishra@users.noreply.github.com>
2023-07-24 02:24:24 -0700

    Update Spider.java

--- src/Spider.java
+++ src/Spider.java
@@ -1,2 +1,107 @@
 public class Spider {
+    private int x; // x position
+    private int y; // y position
+    private Direction direction; // facing direction
+
+    // Enum representing the four possible directions
+    public enum Direction {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    public Spider(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    // Getters and Setters for x, y, and direction
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    // Helper method to move the spider one step forward in the current direction
+    public void moveForward() {
+        switch (direction) {
+            case NORTH:
+                y--;
+                break;
+            case EAST:
+                x++;
+                break;
+            case SOUTH:
+                y++;
+                break;
+            case WEST:
+                x--;
+                break;
+        }
+    }
+
+    // Helper method to turn the spider left
+    public void turnLeft() {
+        switch (direction) {
+            case NORTH:
+                direction = Direction.WEST;
+                break;
+            case EAST:
+                direction = Direction.NORTH;
+                break;
+            case SOUTH:
+                direction = Direction.EAST;
+                break;
+            case WEST:
+                direction = Direction.SOUTH;
+                break;
+        }
+    }
+
+    // Helper method to turn the spider right
+    public void turnRight() {
+        switch (direction) {
+            case NORTH:
+                direction = Direction.EAST;
+                break;
+            case EAST:
+                direction = Direction.SOUTH;
+                break;
+            case SOUTH:
+                direction = Direction.WEST;
+                break;
+            case WEST:
+                direction = Direction.NORTH;
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Spider{" +
+                "x=" + x +
+                ", y=" + y +
+                ", direction=" + direction +
+                '}';
+    }
 }
