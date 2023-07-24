20f67ddbd3defacc5266fe2a086b7a094333d11f
Eeshan Mishra <55258933+EeshanMishra@users.noreply.github.com>
2023-07-24 02:31:36 -0700

    Update RepeatUntilPaintBlueDecorator.java

--- src/RepeatUntilPaintBlueDecorator.java
+++ src/RepeatUntilPaintBlueDecorator.java
@@ -1,1 +1,13 @@
+public class RepeatUntilPaintBlueDecorator extends CodeBlockDecorator{
+    public RepeatUntilPaintBlueDecorator(CodeBlock decoratedCodeBlock) {
+        super(decoratedCodeBlock);
+    }
+
+    @Override
+    public void execute(Spider spider, World grid) {
+        while (!"Blue".equals(grid.getCell(spider.getX(), spider.getY()).getColor())) {
+            decoratedCodeBlock.execute(spider, grid);
+        }
+    }
+}
 
